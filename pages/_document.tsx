b8ed63287978c8afe2dfa1e4b445411d0e81b4f2
Sitch <michael.sitchenko@gmail.com>
2023-04-16 14:54:35 -0400

    disable new relic

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,46 +8,46 @@ import Document, {
 } from 'next/document'
 import { site } from '@/data/siteConfig'
 import i18nextConfig from '@/next-i18next.config'
-import newrelic from 'newrelic'
-import { logger } from '@/lib/logger'
+// import newrelic from 'newrelic'
+// import { logger } from '@/lib/logger'
 
 type NewRelicProps = {
   browserTimingHeader: string
 }
 
 class MyDocument extends Document<NewRelicProps> {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps & NewRelicProps> {
-    const initialProps = await Document.getInitialProps(ctx)
+  // static async getInitialProps(
+  //   ctx: DocumentContext
+  // ): Promise<DocumentInitialProps & NewRelicProps> {
+  //   const initialProps = await Document.getInitialProps(ctx)
 
-    /**
-     * For SSG pages the build is faster than the agent connect cycle
-     * In those cases, let's wait for the agent to connect before getting
-     * the browser agent script.
-     */
-    if (!newrelic.agent.collector.isConnected()) {
-      await new Promise((resolve) => {
-        newrelic.agent.on('connected', resolve)
-      })
-    }
+  //   /**
+  //    * For SSG pages the build is faster than the agent connect cycle
+  //    * In those cases, let's wait for the agent to connect before getting
+  //    * the browser agent script.
+  //    */
+  //   if (!newrelic.agent.collector.isConnected()) {
+  //     await new Promise((resolve) => {
+  //       newrelic.agent.on('connected', resolve)
+  //     })
+  //   }
 
-    const browserTimingHeader = newrelic.getBrowserTimingHeader({
-      hasToRemoveScriptWrapper: true,
-      allowTransactionlessInjection: true,
-    })
+  //   const browserTimingHeader = newrelic.getBrowserTimingHeader({
+  //     hasToRemoveScriptWrapper: true,
+  //     allowTransactionlessInjection: true,
+  //   })
 
-    logger.info('NextJs New Relic redirecting to a page', {
-      application: 'NextJs NewRelic app logging',
-      test: 'Testing logging with Winston',
-      pathname: ctx.pathname,
-    })
+  //   logger.info('NextJs New Relic redirecting to a page', {
+  //     application: 'NextJs NewRelic app logging',
+  //     test: 'Testing logging with Winston',
+  //     pathname: ctx.pathname,
+  //   })
 
-    return {
-      ...initialProps,
-      browserTimingHeader,
-    }
-  }
+  //   return {
+  //     ...initialProps,
+  //     browserTimingHeader,
+  //   }
+  // }
 
   render() {
     const lang =
@@ -140,10 +140,10 @@ class MyDocument extends Document<NewRelicProps> {
             `}</style> */}
 
           {/* NewRelic */}
-          <script
+          {/* <script
             type="text/javascript"
             dangerouslySetInnerHTML={{ __html: this.props.browserTimingHeader }}
-          />
+          /> */}
         </Head>
 
         <body className="loading">
