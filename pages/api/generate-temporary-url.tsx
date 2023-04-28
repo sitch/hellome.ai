d35559dd0954bf3921976e421e905e5e6981961e
sitch <2736163+sitch@users.noreply.github.com>
2023-04-27 22:33:18 -0400

    Update generate-temporary-url.tsx

--- pages/api/generate-temporary-url.tsx
+++ pages/api/generate-temporary-url.tsx
@@ -23,3 +23,31 @@ export default async function handler(
 
   res.status(200).json({ temporaryUrl })
 }
+
+
+
+
+import { NextApiRequest, NextApiResponse } from "next";
+import multiparty from "multiparty";
+
+const uploadImage = async (req: NextApiRequest, res: NextApiResponse) => {
+  const form = new multiparty.Form();
+  const data = await new Promise((resolve, reject) => {
+    form.parse(req, function (err, fields, files) {
+      if (err) reject({ err });
+      resolve({ fields, files });
+    });
+  });
+  console.log(`data: `, JSON.stringify(data));
+
+  res.status(200).json({ success: true });
+};
+
+export default uploadImage;
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+
