74acf8c1f35c6bc9c6b7b1259dde1e345330a801
Sitch <michael.sitchenko@gmail.com>
2023-05-09 17:15:45 -0400

    update

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,12 +4,24 @@
  * @type {import('next-i18next').UserConfig}
  */
 module.exports = {
+  /**
+   * Debug
+   * @link https://www.i18next.com/overview/configuration-options#logging
+   */
+  debug: process.env.NODE_ENV === "development",
+
+  /**
+   * i18n
+   */
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
-    // localeExtension: 'yml',
+
     localeDetection: true,
+    localeExtension: "json",
+    // localeExtension: 'yml',
   },
+
   /**
    * To avoid issues when deploying to some PaaS (vercel)
    */
@@ -17,10 +29,14 @@ module.exports = {
     typeof window === "undefined"
       ? require("path").resolve("./public/locales")
       : "/locales",
-  /**
-   * Development
-   * @link https://www.i18next.com/overview/configuration-options#logging
-   */
+
   reloadOnPrerender: process.env.NODE_ENV === "development",
-  debug: process.env.NODE_ENV === "development",
+
+  /**
+   * @link https://github.com/i18next/next-i18next#6-advanced-configuration
+   */
+  // saveMissing: false,
+  // strictMode: true,
+  // serializeConfig: false,
+  // react: { useSuspense: false }
 }
