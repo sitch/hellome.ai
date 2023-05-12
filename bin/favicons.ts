2de0093bf1cfae92205e88e8b71fb302429fd147
Sitch <michael.sitchenko@gmail.com>
2023-05-11 21:09:24 -0400

    update

--- bin/favicons.ts
+++ bin/favicons.ts
@@ -1,7 +1,6 @@
 import { mkdir, writeFile } from "fs/promises"
 import path from "path"
 
-import chalk from "chalk"
 import favicons, {
   type FaviconFile,
   type FaviconImage,
@@ -12,8 +11,32 @@ import prettier from "prettier"
 // eslint-disable-next-line @limegrass/import-alias/import-alias
 import pwaConfig from "../pwa.config.js"
 
+const colors = {
+  reset: "\\033[0m",
+
+  //text color
+  black: "\\033[30m",
+  red: "\\033[31m",
+  green: "\\033[32m",
+  yellow: "\\033[33m",
+  blue: "\\033[34m",
+  magenta: "\\033[35m",
+  cyan: "\\033[36m",
+  white: "\\033[37m",
+
+  //background color
+  blackBg: "\\033[40m",
+  redBg: "\\033[41m",
+  greenBg: "\\033[42m",
+  yellowBg: "\\033[43m",
+  blueBg: "\\033[44m",
+  magentaBg: "\\033[45m",
+  cyanBg: "\\033[46m",
+  whiteBg: "\\033[47m",
+}
+
 const info = (...args: any[]) => {
-  console.log(chalk.green("[bin/favicons]"), ...args)
+  console.log(`${colors.green}[bin/favicons]${colors.reset}`, ...args)
 }
 
 function templateForComponent({ html }: FaviconResponse) {
